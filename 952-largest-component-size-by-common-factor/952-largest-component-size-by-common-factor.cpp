7c4b60cf5a57f3ee9d9f900788d3c5c8ce6dc485
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-02-17 00:46:06 +0530

    Time: 951 ms (13.05%), Space: 66.7 MB (54.14%) - LeetHub

---
+++ 952-largest-component-size-by-common-factor/952-largest-component-size-by-common-factor.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    vector<int>par;
+    vector<int>R;
+    int find(int a){
+        if(a == par[a]) return a;
+        return par[a] = find(par[a]);
+    }
+    
+    void _union(int a,int b){
+        a = find(a);
+        b = find(b);
+        
+        if(a == b) return;
+        if(R[a]>R[b]) swap(a,b);
+        
+        par[a] = b;
+        R[b] += R[a];
+    }
+    
+    int largestComponentSize(vector<int>& nums) {
+        
+        int size = *max_element(nums.begin(), nums.end());
+        par.resize(size+1);
+        for(int i=0; i<size+1; ++i) par[i]=i;
+        R.resize(size+1,1);
+        
+        for(int i=0; i<nums.size(); ++i){
+            for(int j=2; j<=sqrt(nums[i]); ++j){
+                if(nums[i]%j==0){
+                    _union(nums[i], j);
+                    _union(nums[i]/j, j);
+                }
+            }
+        }
+        int count = 0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int p = find(nums[i]);
+            mp[p]++;
+            count=max(count,mp[p]);
+        }
+        return count;
+    }
+};
