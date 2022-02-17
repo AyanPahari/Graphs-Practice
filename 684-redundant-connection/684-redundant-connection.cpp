125641b46fe5653b2520c4b4b16c8e3760472997
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-02-17 16:09:19 +0530

    Time: 10 ms (55.29%), Space: 8.7 MB (96.22%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     
     vector<int>par,R;
-    
     int find(int a){
         if(a == par[a]) return a;
         return par[a] = find(par[a]);
@@ -13,26 +12,23 @@ public:
         b = find(b);
             
         if(R[b]>R[a]) swap(a,b);
-        
         par[b] = a;
         R[a] += R[b];
     }
     
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        
         int size = edges.size();
         par.resize(size+1);
         for(int i=1;i<=size;i++) par[i] = i;
         R.resize(size+1,1);
         
-        vector<int>res;
         for(int i=0;i<size;i++){
             int a = edges[i][0];
             int b = edges[i][1];
             
-            if(find(a)==find(b)) res = edges[i];
+            if(find(a)==find(b)) return edges[i];
             else _union(a,b);
         }
-        return res;
+        return {};
     }
 };
