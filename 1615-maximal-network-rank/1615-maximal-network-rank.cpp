a8f7ec1b91169e58d3f14c0af7cc026021c1756b
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-03-05 13:14:46 +0530

    Time: 85 ms (83.48%), Space: 29.8 MB (87.72%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<int>degree(n,0);
+        for(int i=0;i<roads.size();i++){
+            degree[roads[i][0]]++;
+            degree[roads[i][1]]++;
+        }
+        vector<vector<int>> adj_mat(n, vector<int>(n, 0));
+        for(int i=0;i<roads.size();i++){
+            adj_mat[roads[i][0]][roads[i][1]] = 1;
+            adj_mat[roads[i][1]][roads[i][0]] = 1;
+        }
+        int maxi = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(adj_mat[i][j] == 1) maxi = max(maxi,degree[i]+degree[j]-1);
+                else maxi = max(maxi, degree[i]+degree[j]);
+            }
+        }
+        return maxi;
+    }
+};
